3a3c843cacc29b084a9f4a608291eea8fea000b8
CompileVibe <siddharthvp2007@gmail.com>
2026-09-12 08:55:11 +0530

    final temp.py

--- temp_mode.py
+++ temp_mode.py
@@ -1,10 +1,9 @@
-choose = int(input("choose the code to run (0/1): "))
+choose = int(input("choose the code to run (0/1/2): "))
 if choose == 0:
     temp = 45.0
     if temp <40:
         print("Cooling ON")
     print("check complete")
-
 elif choose == 1:
     temp = float(input("Enclosure temperature: "))
     if temp < 0:
@@ -16,6 +15,14 @@ elif choose == 1:
     else:
         mode = "shutdown"
     print("operating mode = ",mode)
-
+elif choose == 2:
+    marks = 95
+    if marks >=40:
+        grade = "pass"
+    elif marks >= 95:
+        grade = "distinction"
+    else:
+        grade = "fail"
+    print(grade)
 else:
     print("Invalid choice. Please choose 0 or 1.")  
